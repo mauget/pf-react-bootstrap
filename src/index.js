06de108ada920bcb46dac0b7ad49bfb722700cdf
mauget <mauget@mindspring.com>
2017-07-31 12:21:46 -0400

    Changed build to create a static snapshot for running directly

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Changed to static snapshot for build
+// ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <App/>,
+    document.getElementById('root')
+);
+
+
 registerServiceWorker();
