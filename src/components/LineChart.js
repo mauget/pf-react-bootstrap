0b5969df64d4c9a7bacc7038414b2f082c2ae4b9
mauget <mauget@mindspring.com>
2017-07-31 10:23:34 -0400

    Removed unused code in LineChart.js

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -7,20 +7,17 @@ export class LineChart extends Component {
 
     constructor(props) {
         super(props);
-        this.label = props.label;
         this.id = props.id;
-        this.defaultChecked = props.defaultChecked;
-        this.data = props.src;
+        this.src = props.src;
 
         this.handleClick = this.handleClick.bind(this);
-
     }
 
     handleClick(ev) {
     }
 
     render() {
-        return <C3Chart data={this.data} />;
+        return <C3Chart data={this.src} />;
     }
 
 }
