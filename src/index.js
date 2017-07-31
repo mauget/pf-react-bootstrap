eb817384f32c90a19ac98168247b5547bf913d54
mauget <mauget@mindspring.com>
2017-07-31 12:27:23 -0400

    Reverted from static snapshot for running directly - CSS issues

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// Changed to static snapshot for build
-// ReactDOM.render(<App />, document.getElementById('root'));
-render(
-    <App/>,
-    document.getElementById('root')
-);
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
